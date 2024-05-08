7d9422ee74b18b8fe84816d53bf884661652b489
rowanxx16 <rowanxxx16@gmail.com>
2024-05-08 22:06:03 +0300

    commit

--- footer.php
+++ footer.php
@@ -52,8 +52,8 @@
           <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
             <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
             <p><i class="fas fa-home mr-3"></i> Khobar, Saudi Arabia</p>
-            <p><i class="fas fa-envelope mr-3"></i> Gamerstore@gmail.com</p>
-            <p><i class="fas fa-phone mr-3"></i> +966 510111111</p>
+            <p><i class="fas fa-envelope mr-3"></i> pizzaTime@gmail.com</p>
+            <p><i class="fas fa-phone mr-3"></i> +966 554432782</p>
           </div>
           <!-- Grid column -->
         </div>
